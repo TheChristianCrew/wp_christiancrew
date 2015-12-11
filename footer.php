3043b6d17f2e1a5b73d9dd6bdb86bf3becbe5727
Keith Brinks <keithbrinks@me.com>
2015-12-11 10:01:53 -0500

    Begin building header

--- footer.php
+++ footer.php
@@ -1,29 +1,4 @@
-	<footer id="cc-site-footer">
-
-		<div class="cc-inner-wrap">
-
-			<a class="cc-tag" href="<?php bloginfo('url'); ?>">&dagger;&lt;CC&gt;&lt; <?php bloginfo('name'); ?></a>
-			<a class="solideogloria-link" href="http://en.wikipedia.org/wiki/Soli_Deo_gloria" title="Glory to God Alone">Soli Deo Gloria</a>
-
-		</div>
-
-	</footer>
-
 <?php wp_footer(); ?>
-
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-18273165-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
 
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,36 +1,26 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
-
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
 	<?php wp_head(); ?>
-
 </head>
 <body <?php body_class(); ?>>
 
-	<header id="cc-site-header">
-
+	<header class="cc-site-header">
 		<div class="cc-inner-wrap">
-
-			<a id="cc-site-banner" href="<?php bloginfo('url'); ?>"><img src="<?php echo get_root_blog_url(); ?>/cc-global-theme/v3/img/cc-site-banner.png" alt="Christian Crew Gaming - A faith based gaming community" /></a>
-
-			<nav id="cc-site-nav">
+			<?php
+				if ( is_front_page() && is_home() ) : ?>
+					<h1 class="cc-site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php else : ?>
+					<p class="cc-site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php endif;
+			?>
+			<nav class="cc-site-nav">
 				<?php wp_nav_menu(array('theme_location' => 'site_nav')); ?>
 			</nav>
-
-			<ul id="cc-social-links">
-				<li class="twitter"><a href="http://twitter.com/ccgamingonline"><i class="fa fa-twitter-square"></i></a></li>
-				<li class="facebook"><a href="http://facebook.com/ccgaming"><i class="fa fa-facebook-square"></i></a></li>
-				<li class="steam"><a href="http://steamcommunity.com/groups/christiancrewgaming"><i class="fa fa-steam-square"></i></a></li>
-				<li class="feed"><a href="http://forums.ccgaming.com/syndication.php?t=1&fid=82"><i class="fa fa-rss-square"></i></a></li>
-			</ul>
-
 		</div>
-
 	</header>
