7e2cdbd75e427854bab6e2adf41cadd38ca76313
Keith Brinks <keithbrinks@me.com>
2016-01-01 23:26:38 -0600

    Tweak home intro

--- home.php
+++ home.php
@@ -2,10 +2,10 @@
 
 <div class="page-wrap">
 	<div class="home-intro">
-		<p>Welcome to Christian Crew Gaming.<br />
-			Our mission is to be a gaming community that has<br />
-			a clean environment, great fellowship,<br />
-			and exciting gameplay.</p>
+		<div class="home-intro-inner">
+			<h2>Welcome to Christian Crew Gaming.</h2>
+			<p>Our mission is to be a gaming community that provides a clean environment, great fellowship, and exciting gameplay.</p>
+		</div>
 	</div>
 	<div class="page-container home-container">
 		News blurb
