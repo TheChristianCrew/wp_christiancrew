6f2796a34805c5e18b6fcb4b7c9d1d7d6174a5e9
Keith Brinks <keithbrinks@me.com>
2016-01-01 22:04:37 -0600

    Move parallax code for home page to home template

--- footer.php
+++ footer.php
@@ -2,14 +2,6 @@
 	<span id="its733191"><a href="http://www.teamspeak3.com/">teamspeak</a> Hosting by TeamSpeak3.com</span><script type="text/javascript" src="http://view.light-speed.com/teamspeak3.php?IP=ts.ccgaming.com&PORT=9987&QUERY=10011&UID=733191&display=block&font=11px&background=transparent&server_info_background=transparent&server_info_text=%23000000&server_name_background=transparent&server_name_text=%23000000&info_background=transparent&channel_background=transparent&channel_text=%23000000&username_background=transparent&username_text=%23000000"></script>
 </div>
 
-<?php if (is_front_page() && is_home() ) : ?>
-	<ul id="scene" class="scene-home">
-		<li class="layer" data-depth="0.20"><div class="background"></div></li>
-		<li class="layer" data-depth="0.40"><div class="chopper"></div></li>
-		<li class="layer" data-depth="0.90"><div class="chopper-2"></div></li>
-	</ul>
-<?php endif; ?>
-
 <?php wp_footer(); ?>
 
 </body>
--- home.php
+++ home.php
@@ -1,3 +1,9 @@
 <?php get_header(); ?>
 
+<ul id="scene" class="scene-home">
+	<li class="layer" data-depth="0.20"><div class="background"></div></li>
+	<li class="layer" data-depth="0.40"><div class="chopper"></div></li>
+	<li class="layer" data-depth="0.90"><div class="chopper-2"></div></li>
+</ul>
+
 <?php get_footer(); ?>
