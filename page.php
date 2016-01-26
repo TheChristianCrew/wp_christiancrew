20b7e1d06918d34a1d35fb1de4c52b6fcb6aecb5
Keith Brinks <keithbrinks@me.com>
2016-01-25 22:07:55 -0600

    Implement initial base page structure

--- page.php
+++ page.php
@@ -1,57 +1,15 @@
 <?php get_header() ?>
 
-	<?php
-		// Get page ancestors
-		// Have to store in variable for PHP < 5.4
-		$ancestors = get_post_ancestors($post);
-	?>
+<div class="page-wrap">
 
-	<section id="cc-page-title">
-		<div class="cc-inner-wrap">
-			<h1><?php the_title(); ?></h1>
-		</div>
-	</section>
+	<h1 class="page-title"><?php the_title(); ?></h1>
 
-	<div class="cc-container">
+	<div class="page-container">
 
-		<div class="cc-inner-wrap grid">
-
-			<?php if (is_active_sidebar(get_post($ancestors[0])->post_name .'_page_sidebar') || is_active_sidebar(get_post($ancestors[1])->post_name .'_page_sidebar')) : ?>
-
-				<div class="col-8 has-sidebar">
-
-					<?php if (has_nav_menu(get_post($ancestors[0])->post_name .'_page_nav')) : ?>
-						<nav id="cc-page-nav">
-							<?php wp_nav_menu(array('theme_location' => get_post($ancestors[0])->post_name .'_page_nav')); ?>
-						</nav>
-					<?php elseif (has_nav_menu(get_post($ancestors[1])->post_name .'_page_nav')) : ?>
-						<nav id="cc-page-nav">
-							<?php wp_nav_menu(array('theme_location' => get_post($ancestors[1])->post_name .'_page_nav')); ?>
-						</nav>
-					<?php endif; ?>
-
-					<?php get_template_part('partials/page-loop.php') ?>
-
-				</div>
-
-				<div class="col-4">
-
-					<?php get_sidebar(); ?>
-
-				</div>
-
-			<?php else : ?>
-
-				<div class="col-12">
-
-					<?php get_template_part('partials/page-loop.php') ?>
-
-				</div>
-
-			<?php endif; ?>
-
-		</div>
+		<?php get_template_part('partials/page', 'loop') ?>
 
 	</div>
+
+</div>
 
 <?php get_footer(); ?>
