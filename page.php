4f0e783affa9b8a1212c95ba963480688d2ae5e0
Keith Brinks <keithbrinks@me.com>
2016-02-05 16:45:01 -0700

    Grid fixes + divisions page template

--- page.php
+++ page.php
@@ -6,8 +6,10 @@
 
 		<h1 class="page-title"><?php the_title(); ?></h1>
 
-		<div class="page-container">
-			<?php get_template_part('partials/page', 'loop') ?>
+		<div class="page-container grid">
+			<div class="grid-4-4">
+				<?php get_template_part('partials/page', 'loop') ?>
+			</div>
 		</div>
 
 	</article>
