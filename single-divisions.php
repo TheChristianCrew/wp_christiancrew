c4ff1eb1b35c9246a84288037701f0c04f64dbee
Keith Brinks <keithbrinks@me.com>
2016-08-15 22:30:21 -0400

    Blur effect over background images?

--- single-divisions.php
+++ single-divisions.php
@@ -12,35 +12,38 @@ get_header() ?>
 		<h1 class="page-title"><?php the_title(); ?></h1>
 
 		<div class="container">
-			<div class="grid grid-pad">
-	      <div class="col-1-1">
-					<?php if ($post->post_name != 'whatelse') : ?>
-						<ul class="division-links">
-							<li><a href="<?php echo get_post_meta($post->ID, '_steam_store_link', true); ?>"><i class="fa fa-shopping-cart fa-fw"></i> Steam Store Page</a></li>
-							<li><a href="<?php echo phpbb::$url .'viewforum.php?f='. get_post_meta($post->ID, '_forum_id', true); ?>"><i class="fa fa-comments fa-fw"></i> Forum Section</a></li>
-						</ul>
-					<?php endif; ?>
-				  <?php get_template_part('partials/page', 'loop') ?>
-	      </div>
+			<div class="glass-bg"></div>
+			<div class="glass-bg-container">
+				<div class="grid grid-pad">
+		      <div class="col-1-1">
+						<?php if ($post->post_name != 'whatelse') : ?>
+							<ul class="division-links">
+								<li><a href="<?php echo get_post_meta($post->ID, '_steam_store_link', true); ?>"><i class="fa fa-shopping-cart fa-fw"></i> Steam Store Page</a></li>
+								<li><a href="<?php echo phpbb::$url .'viewforum.php?f='. get_post_meta($post->ID, '_forum_id', true); ?>"><i class="fa fa-comments fa-fw"></i> Forum Section</a></li>
+							</ul>
+						<?php endif; ?>
+					  <?php get_template_part('partials/page', 'loop') ?>
+		      </div>
+				</div>
+				<?php if ($post->post_name != 'whatelse') : ?>
+					<div class="grid grid-pad">
+						<div class="col-1-3">
+							<h2>Division Leaders</h2>
+							<?php echo do_shortcode( '[phpbb-list-members users="'. get_post_meta( $post->ID, '_division_leader_ids', true ) .'"]' ); ?>
+						</div>
+						<div class="col-2-3">
+							<h2>Division Info</h2>
+							<section class="division_info">
+								Division info.
+							</section>
+						</div>
+					</div>
+					<div class="grid grid-pad">
+						<h2>Recent Topics</h2>
+						<?php echo do_shortcode( '[phpbb-list-topics limit="4" forum_id="'. get_post_meta( $post->ID, '_forum_id', true ) .'"]' ); ?>
+					</div>
+				<?php endif; ?>
 			</div>
-			<?php if ($post->post_name != 'whatelse') : ?>
-				<div class="grid grid-pad">
-					<div class="col-1-3">
-						<h2>Division Leaders</h2>
-						<?php echo do_shortcode( '[phpbb-list-members users="'. get_post_meta( $post->ID, '_division_leader_ids', true ) .'"]' ); ?>
-					</div>
-					<div class="col-2-3">
-						<h2>Division Info</h2>
-						<section class="division_info">
-							Division info.
-						</section>
-					</div>
-				</div>
-				<div class="grid grid-pad">
-					<h2>Recent Topics</h2>
-					<?php echo do_shortcode( '[phpbb-list-topics limit="4" forum_id="'. get_post_meta( $post->ID, '_forum_id', true ) .'"]' ); ?>
-				</div>
-			<?php endif; ?>
 		</div>
 
 	</article>
