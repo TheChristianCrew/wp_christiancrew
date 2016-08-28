af260f619b4966e7ecdba216598f41caaf29ff85
Keith Brinks <keithbrinks@me.com>
2016-08-28 09:27:20 -0400

    Update Font Awesome caching version number

--- functions.php
+++ functions.php
@@ -80,7 +80,7 @@ function wp_christiancrew_scripts() {
 	wp_enqueue_style('theme-style', get_stylesheet_uri(), false, '4.0');
 
 	// Load FontAwesome
-	wp_enqueue_style( 'fontawesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css', false, '4.6.1' );
+	wp_enqueue_style( 'fontawesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css', false, '4.6.3' );
 
 	// Load main JavaScript file
 	wp_enqueue_script( 'cc-script', get_template_directory_uri() . '/assets/js/cc.min.js', array( 'jquery' ), '12312015', true );
