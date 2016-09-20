20db5ed1b90542766290b260bf6e7d79cc2a229d
Keith Brinks <keithbrinks@me.com>
2016-09-20 07:44:59 -0400

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -6,7 +6,7 @@
 		$ancestors = get_post_ancestors($post);
 	?>
 
-	<?php if (function_exists('dynamic_sidebar') && dynamic_sidebar(get_post($ancestors[0])->post_name .'_page_sidebar') || dynamic_sidebar(get_post($ancestors[1])->post_name .'_page_sidebar')) : ?>
+	<?php if (function_exists('dynamic_sidebar') && dynamic_sidebar(get_post($ancestors[0])->post_name .'_page_sidebar') || dynamic_sidebar(get_post($ancestors[1])->post_name .'_page_sidebar')) : else : ?>
 	
 		<!-- If no dynamic sidebar exists, use the content below -->
 		Error loading <?php echo $post->post_name; ?> sidebar
